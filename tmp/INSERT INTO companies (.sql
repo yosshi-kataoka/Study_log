e3a7b3aa25a7b2ba1be6e91a7015292d35b2be6b
yosshi-kataoka <135479938+yosshi-kataoka@users.noreply.github.com>
2023-07-08 21:58:08 +0900

    Delete tmp directory

--- tmp/INSERT INTO companies (.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO companies (
-    name,
-    establishment_date,
-    founder
-) VALUES (
-  'mercari inc',
-  '2013-02-01',
-  'Shintaro Yamada'
-);
--- tmp/INSERT INTO companies2(.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO companies(
-    name,
-    establishment_date,
-    founder
-) VALUES(
-    'National',
-    '1987-03-01',
-    'Kounosuke Matsushita'
-);
